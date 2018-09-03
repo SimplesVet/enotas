0bada525d816342506a2ac77cd0536c7f5755ca4
Mateus Neves <mateus@mateusneves.com.br>
2018-09-03 11:30:31 -0300

    ajustes de psr; nova função de consulta por razão social; atualiza versão

--- src/empresaApi.php
+++ src/empresaApi.php
@@ -1,89 +1,111 @@
 <?php
-	namespace eNotasGW\Api;
+    namespace eNotasGW\Api;
 
-	class empresaApi extends eNotasGWApiBase {
-		public function __construct($proxy) {
-			parent::__construct($proxy);
-		}
+class empresaApi extends eNotasGWApiBase
+{
+    public function __construct($proxy)
+    {
+        parent::__construct($proxy);
+    }
 
 
-		/**
-		 * Insere ou atualiza uma empresa
-		 *
-		 * @param mixed $dados dados da empresa que a serem utilizados na inserção ou atualização
-		 */
-		public function consultar($idEmpresa)
-		{
-			return $this->callOperation(array(
-				'method' => 'GET',
-				'path' => '/empresas/{empresaId}',
-				'parameters' => array(
-					'path' => array(
-						'empresaId' => $idEmpresa
-					)
-				)
-			));
-		}
+    /**
+     * Consultar empresa por ID único
+     *
+     * @param string $idEmpresa
+     */
+    public function consultar($idEmpresa)
+    {
+        return $this->callOperation(array(
+                'method' => 'GET',
+                'path' => '/empresas/{empresaId}',
+                'parameters' => array(
+                    'path' => array(
+                        'empresaId' => $idEmpresa
+                    )
+                )
+            ));
+    }
 
-		/**
-		 * Insere ou atualiza uma empresa
-		 *
-		 * @param mixed $dados dados da empresa que a serem utilizados na inserção ou atualização
-		 */
-		public function inserirAtualizar($dados) {
-			return $this->callOperation(array(
-				'method' => 'POST',
-				'path' => '/empresas',
-				'parameters' => array(
-					'body' => $dados
-				)
-			));
-		}
+    /**
+     * Listar empresa por sua Razão Social
+     *
+     * @param string $razaoSocial
+     */
+    public function consultarPorRazaoSocial($razaoSocial)
+    {
+        return $this->callOperation(array(
+                'method' => 'GET',
+                'path' => '/empresas?pageNumber=0&pageSize=1&searchBy=Razao_Social&searchTerm={razaoSocial}',
+                'parameters' => array(
+                    'path' => array(
+                        'razaoSocial' => $razaoSocial
+                    )
+                )
+            ));
+    }
 
-		/**
-		 * Atualiza a logo da empresa
-		 *
-		 * @param string $idEmpresa id da empresa para a qual a nota será emitida
-		 * @param fileParameter $file imagem a ser utilizada como logo.
-		 */
-		public function atualizarLogo($idEmpresa, $file) {
-			$this->callOperation(array(
-				'method' => 'POST',
-				'decodeResponse' => FALSE,
-				'path' => '/empresas/{empresaId}/logo',
-				'parameters' => array(
-					'path' => array(
-					  'empresaId' => $idEmpresa
-					),
-					'form' => array(
-						'logotipo' => $file
-					)
-				)
-			));
-		}
+    /**
+     * Insere ou atualiza uma empresa
+     *
+     * @param mixed $dados dados da empresa que a serem utilizados na inserção ou atualização
+     */
+    public function inserirAtualizar($dados)
+    {
+        return $this->callOperation(array(
+                'method' => 'POST',
+                'path' => '/empresas',
+                'parameters' => array(
+                    'body' => $dados
+                )
+            ));
+    }
 
-		/**
-		 * Atualiza o certificado digital da empresa
-		 *
-		 * @param string $idEmpresa id da empresa para a qual a nota será emitida
-		 * @param fileParameter $file arquivo do certificado.
-		 * @param string $pass senha do certificado.
-		 */
-		public function atualizarCertificado($idEmpresa, $file, $pass) {
-			$this->callOperation(array(
-				'method' => 'POST',
-				'decodeResponse' => FALSE,
-				'path' => '/empresas/{empresaId}/certificadoDigital',
-				'parameters' => array(
-					'path' => array(
-					  'empresaId' => $idEmpresa
-					),
-					'form' => array(
-						'arquivo' => $file,
-						'senha' => $pass
-					)
-				)
-			));
-		}
-	}
-?>
+    /**
+     * Atualiza a logo da empresa
+     *
+     * @param string $idEmpresa id da empresa para a qual a nota será emitida
+     * @param fileParameter $file imagem a ser utilizada como logo.
+     */
+    public function atualizarLogo($idEmpresa, $file)
+    {
+        $this->callOperation(array(
+                'method' => 'POST',
+                'decodeResponse' => false,
+                'path' => '/empresas/{empresaId}/logo',
+                'parameters' => array(
+                    'path' => array(
+                      'empresaId' => $idEmpresa
+                    ),
+                    'form' => array(
+                        'logotipo' => $file
+                    )
+                )
+            ));
+    }
+
+    /**
+     * Atualiza o certificado digital da empresa
+     *
+     * @param string $idEmpresa id da empresa para a qual a nota será emitida
+     * @param fileParameter $file arquivo do certificado.
+     * @param string $pass senha do certificado.
+     */
+    public function atualizarCertificado($idEmpresa, $file, $pass)
+    {
+        $this->callOperation(array(
+                'method' => 'POST',
+                'decodeResponse' => false,
+                'path' => '/empresas/{empresaId}/certificadoDigital',
+                'parameters' => array(
+                    'path' => array(
+                      'empresaId' => $idEmpresa
+                    ),
+                    'form' => array(
+                        'arquivo' => $file,
+                        'senha' => $pass
+                    )
+                )
+            ));
+    }
+}
